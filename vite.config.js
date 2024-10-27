fc091724fda56cd3a9e1e62592a2b636684db3ca
homme_de_valeur <91547336+hommedevaleur@users.noreply.github.com>
2024-10-27 13:11:25 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   build: {
-    rollupOptions: {
-      external: [
-        '@firebase/auth', // Mark @firebase/auth as external
-        '@firebase/analytics', // Add other Firebase packages if necessary
-      ],}},
-  plugins: [react()],
+    outDir: 'dist'
+  }
 });
+
